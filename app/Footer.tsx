79f34d49a88106bfa818548512225a7793035aa6
mohsalim <moharts2000@gmail.com>
2023-11-09 18:04:51 +0300

    lets start this

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -63,150 +63,150 @@ function Footer() {
        </div> 
      </div>
  
-     <div className="mt-16 grid gap-16 border-t border-gray-100 pt-16 md:grid-cols-2 lg:grid-cols-6">
-  <div className="md:col-span-2 lg:col-span-1">
-    <div className="text-center grid sm:text-left">
-      <p className="text-lg font-medium text-gray-900 dark:text-orange-400">About Us</p>
-      <ul className="mt-8 space-y-4 text-sm">
-        <li>
-          <a className="text-gray-700 transition dark:text-white hover:text-gray-700/75" onClick={handleHistoryClick}>
-            Company History
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleManClick}>
-            Management
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleClick}>
-            Presenters
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleScholarsClick}>
-            Sheikhs/scholars
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div className="md:col-span-2 lg:col-span-1">
-    <div className="text-center grid sm:text-left">
-      <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Our Services</p>
-      <ul className="mt-8 space-y-4 text-sm">
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
-            News
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
-            Islamic Education
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 cursor-pointer dark:text-white transition hover:text-gray-700/75" onClick={handleAdvertClick}>
-            Advertisements
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
-            Announcements
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="/">
-            Donations
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div className="md:col-span-2 lg:col-span-1">
-    <div className="text-center grid sm:text-left">
-      <p className="text-lg font-medium text-gray-900 dark:text-orange-400">Resources</p>
-      <ul className="mt-8 space-y-4 text-sm">
-        <li>
-          <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75">
-            Weekly Schedule
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
-            Quotes
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition cursor-pointer hover:text-gray-700/75" onClick={handleBusinessClick}>
-            Iqra Biashara
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div className="md:col-span-2 lg:col-span-1">
-    <div className="text-center grid sm:text-left">
-      <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Helpful Links</p>
-      <ul className="mt-8 space-y-4 text-sm">
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
-            FAQs
-          </a>
-        </li>
-        <li>
-          <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
-            Support
-          </a>
-        </li>
-        <li>
-          <a className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end" href="sms:+254727444454">
-            <span className="text-gray-700 dark:text-white transition group-hover:text-gray-700/75">Live Chat</span>
-            <span className="relative flex h-2 w-2">
-              <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-              <span className="relative inline-flex h-2 w-2 rounded-full bg-red-500"></span>
-            </span>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div className="md:col-span-2 lg:col-span-4">
-    <div className="text-center md:text-left items-center">
-      <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Stay in Touch</p>
-      <div className="mx-auto mt-4 max-w-md sm:ms-0">
-        <p className="leading-relaxed dark:text-white text-gray-700 ltr:sm:text-left rtl:sm:text-right">
-          IQRA FM, The station with a difference. We love you for the sake of Allah Subhanahu Wataalah.
-        </p>
-        <form action="https://getform.io/f/acf85c8f-60e6-46f0-bbc7-33461ad39d07" method="POST" className="mt-4">
-          <div className="flex flex-col gap-4 sm:flex-row lg:flex-col lg:items-start">
-            <input
-              className="w-full rounded-full border-gray-400 px-6 py-3 shadow-sm"
-              type="email"
-              name="email"
-              placeholder="Enter your email"
-              required
-            />
-            <input
-              className="w-full rounded-full border-gray-400 px-6 py-3 shadow-sm"
-              type="text"
-              name="message"
-              placeholder="Enter your message"
-              required
-            />
-            <button
-              className="block rounded-full bg-indigo-500 px-8 py-3 font-medium text-white transition hover:bg-indigo-600"
-              type="submit"
-            >
-              Send
-            </button>
+     <div className="mt-16 grid gap-16 border-t border-gray-100 pt-16 md:grid-cols-6 lg:grid-cols-6">
+      <div className="md:col-span-2 lg:col-span-1">
+        <div className="text-center grid sm:text-left">
+          <p className="text-lg font-medium text-gray-900 dark:text-orange-400">About Us</p>
+          <ul className="mt-8 space-y-4 text-sm">
+            <li>
+              <a className="text-gray-700 transition dark:text-white hover:text-gray-700/75" onClick={handleHistoryClick}>
+                Company History
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleManClick}>
+                Management
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleClick}>
+                Presenters
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75" onClick={handleScholarsClick}>
+                Sheikhs/scholars
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-1">
+        <div className="text-center grid sm:text-left">
+          <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Our Services</p>
+          <ul className="mt-8 space-y-4 text-sm">
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
+                News
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
+                Islamic Education
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 cursor-pointer dark:text-white transition hover:text-gray-700/75" onClick={handleAdvertClick}>
+                Advertisements
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
+                Announcements
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="/">
+                Donations
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-1">
+        <div className="text-center grid sm:text-left">
+          <p className="text-lg font-medium text-gray-900 dark:text-orange-400">Resources</p>
+          <ul className="mt-8 space-y-4 text-sm">
+            <li>
+              <a className="text-gray-700 cursor-pointer transition dark:text-white hover:text-gray-700/75">
+                Weekly Schedule
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="https://twitter.com/iqrafm?lang=en" target="_blank">
+                Quotes
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition cursor-pointer hover:text-gray-700/75" onClick={handleBusinessClick}>
+                Iqra Biashara
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-1">
+        <div className="text-center grid sm:text-left">
+          <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Helpful Links</p>
+          <ul className="mt-8 space-y-4 text-sm">
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a className="text-gray-700 dark:text-white transition hover:text-gray-700/75" href="">
+                Support
+              </a>
+            </li>
+            <li>
+              <a className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end" href="sms:+254727444454">
+                <span className="text-gray-700 dark:text-white transition group-hover:text-gray-700/75">Live Chat</span>
+                <span className="relative flex h-2 w-2">
+                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+                  <span className="relative inline-flex h-2 w-2 rounded-full bg-red-500"></span>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="md:col-span-2 lg:col-span-4">
+        <div className="text-center md:text-left items-center">
+          <p className="text-lg font-medium dark:text-orange-400 text-gray-900">Stay in Touch</p>
+          <div className="mx-auto mt-4 max-w-md sm:ms-0">
+            <p className="leading-relaxed dark:text-white text-gray-700 ltr:sm:text-left rtl:sm:text-right">
+              IQRA FM, The station with a difference. We love you for the sake of Allah Subhanahu Wataalah.
+            </p>
+            <form action="https://getform.io/f/acf85c8f-60e6-46f0-bbc7-33461ad39d07" method="POST" className="mt-4">
+              <div className="flex flex-col gap-4 sm:flex-row lg:flex-col lg:items-start">
+                <input
+                  className="w-full rounded-full border-gray-400 px-6 py-3 shadow-sm"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  required
+                />
+                <input
+                  className="w-full rounded-full border-gray-400 px-6 py-3 shadow-sm"
+                  type="text"
+                  name="message"
+                  placeholder="Enter your message"
+                  required
+                />
+                <button
+                  className="block rounded-full bg-indigo-500 px-8 py-3 font-medium text-white transition hover:bg-indigo-600"
+                  type="submit"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
     </div>
-  </div>
-</div>
- 
+    
      <div
        className="mt-16 border-t border-gray-100 pt-6 sm:flex sm:items-center sm:justify-between"
      >
