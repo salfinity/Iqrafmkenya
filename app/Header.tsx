15ea36ddb465af8e01d94347e375dd8fd729b1f2
mohsalim <moharts2000@gmail.com>
2023-11-09 12:48:08 +0300

    font size

--- app/Header.tsx
+++ app/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
     <div className='grid grid-cols-3 p-10 items-center'>
      <Bars3Icon className='h-8 w-8 cursor-pointer' />
      <Link href="/" prefetch={false}>
-      <h1 className='font-serif md:text-4xl text-xl text-center'>
+      <h1 className='font-serif text-xl md:text-4xl text-center'>
        <span className='underline decoration-6 
        decoration-orange-400'>IQRA-FM</span> {" "}
         Kenya</h1>
