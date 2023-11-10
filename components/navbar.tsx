f1784615db0d4c541204e17493479f5ec77259bc
mohsalim <moharts2000@gmail.com>
2023-11-10 18:55:40 +0300

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
 
   return ( 
     <div className="max-w-7xl">
-       <h1 className='font-serif text-2xl md:text-4xl text-center mt-6 mb-6'>
+       <h1 className='font-serif text-xl md:text-4xl text-center mt-6 mb-6'>
        <span className='underline decoration-6 
        decoration-orange-400'>WELCOME TO</span> {" "}
         IQRA BIASHARA</h1>
