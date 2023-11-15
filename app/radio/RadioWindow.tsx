27d94a9a63a2afd8d81860829796999206a3808b
mohsalim <moharts2000@gmail.com>
2023-11-16 01:19:51 +0300

    all

--- app/radio/RadioWindow.tsx
+++ app/radio/RadioWindow.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import RadioButton from "./RadioButton"
 
 function RadioWindow() {
-  const imageWidth = 330; // Set a reasonable width for your images
+  const imageWidth = 360; // Set a reasonable width for your images
   const imageHeight = 600; 
   
   return (
    <div className="relative container box-border border-4 border-slate-500 items-center 
-    grid grid-cols-4 gap-2 max-w-6xl mx-auto overflow-clip cursor-pointer bg-slate-300 dark:bg-slate-700">
+    grid grid-cols-4 gap-4 max-w-6xl mx-auto overflow-clip cursor-pointer bg-slate-300 dark:bg-slate-700">
      <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" >
      <RadioButton />
      </div>
-       <div className="w-full rounded m-2 scale-125 dark:blur-sm hover:shadow-xl 
+       <div className="w-full rounded m-0 scale-125 dark:blur-sm hover:shadow-xl 
        transition-all duration-200 ease-in-out">
            <Image 
            layout="fixed"
@@ -44,7 +44,7 @@ function RadioWindow() {
            alt="image"
           />
        </div>
-       <div className="w-full rounded m-2 dark:scale-125 blur-sm dark:blur-none hover:shadow-xl
+       <div className="w-full rounded scale-125 ml-auto dark:scale-100 blur-sm dark:blur-none hover:shadow-xl
        transition-all duration-200 ease-in-out">
            <Image 
            layout="fixed"
