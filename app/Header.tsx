6e3e1aae1a08ddebb11a22dd4443b2900933ff62
mohsalim <moharts2000@gmail.com>
2023-11-18 13:25:34 +0300

    new one

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,16 +1,24 @@
+"use client";
+
 import NavlLinks from './NavLinks';
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBox from './SearchBox';
 import DarkModeButton from './DarkModeButton';
 import RadioWindow from './radio/RadioWindow';
 
-function Header() {
+function Header() { 
+  const [subscribeVisible, setSubscribeVisible] = useState(false);
+
+  const toggleSubscribe = () => {
+    setSubscribeVisible(!subscribeVisible);
+  };
+  
   return (
    <header>
     <div className='grid grid-cols-3 p-10 items-center'>
-     <Bars3Icon className='h-8 w-8 cursor-pointer' />
+     <Bars3Icon className='h-8 w-8 cursor-pointer' onClick={toggleSubscribe}/>
      <Link href="/" prefetch={false}>
       <h1 className='font-serif text-xl md:text-4xl text-center'>
        <span className='underline decoration-6 
@@ -22,13 +30,24 @@ function Header() {
      space-x-2'>
       {/* DarkModeButton */}
       <DarkModeButton />
-
-      {/* Subscribe button */}
-     <button className='hidden md:inline bg-slate-900 
+      <button className='hidden md:inline bg-slate-900 
       text-white px-4 lg:px-8 ly-2 lg:py-4 rounded-full
        dark:bg-slate-800'>
         Subscribe Now
       </button>
+
+     {/* Subscribe button dropdown */}
+     {/* Subscribe button dropdown */}
+     {subscribeVisible && (
+          <div
+            className='absolute top-12 left-0 bg-white p-4 shadow-md rounded-md md:hidden'
+            onClick={toggleSubscribe}
+          >
+            <button className='block bg-slate-900 text-white px-4 lg:px-8 ly-2 lg:py-4 rounded-full dark:bg-slate-800'>
+              Subscribe Now
+            </button>
+          </div>
+        )}
      </div>
     </div>
     {/* Radio frequency window */}
--- app/radio/RadioWindow.tsx
+++ app/radio/RadioWindow.tsx
@@ -2,16 +2,16 @@ import Image from "next/image";
 import RadioButton from "./RadioButton"
 
 function RadioWindow() {
-  const imageWidth = 360; // Set a reasonable width for your images
+  const imageWidth = 400; // Set a reasonable width for your images
   const imageHeight = 600; 
   
   return (
    <div className="relative container box-border border-4 border-slate-500 items-center 
-    grid grid-cols-4 gap-4 max-w-6xl mx-auto overflow-clip cursor-pointer bg-slate-300 dark:bg-slate-700">
+    grid grid-cols-4 gap-4 max-w-5xl md:max-w-6xl mx-auto overflow-clip cursor-pointer bg-slate-300 dark:bg-slate-700">
      <div className="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" >
      <RadioButton />
      </div>
-       <div className="w-full rounded m-0 scale-125 dark:blur-sm hover:shadow-xl 
+     <div className="w-full rounded m-0 scale-150 dark:scale-125 hover:shadow-xl
        transition-all duration-200 ease-in-out">
            <Image 
            layout="fixed"
@@ -21,8 +21,11 @@ function RadioWindow() {
            src="/radiowindow/aqsaday.jpeg"
            alt="image"
            />
+           <div className='absolute inset-0 bg-black opacity-0 dark:opacity-30 z-50'></div>
        </div>
-       <div className="w-full rounded m-2 dark:scale-125 blur-sm dark:blur-none hover:shadow-xl
+       {/* <div className="w-full rounded m-2 scale-125 dark:scale-125 blur-xl dark:blur-none hover:shadow-xl */}
+       {/* transition-all duration-200 ease-in-out"> */}
+         <div className="w-full h-auto rounded m-0 scale-125 dark:scale-125 hover:shadow-xl
        transition-all duration-200 ease-in-out">
            <Image 
            layout="fixed"
@@ -32,8 +35,10 @@ function RadioWindow() {
            src="/radiowindow/madinanight.jpeg"
            alt="image"
            />
+
+           <div className='absolute inset-0 bg-black opacity-70 dark:opacity-0 z-50'></div>
        </div>
-       <div className="w-full rounded m-2 scale-125 dark:blur-sm hover:shadow-xl
+       <div className="w-full rounded m-0 scale-150 dark:scale-125 hover:shadow-xl
        transition-all duration-200 ease-in-out">
            <Image 
            layout="fixed"
@@ -43,10 +48,13 @@ function RadioWindow() {
            src="/radiowindow/madinaday.jpeg"
            alt="image"
           />
+          <div className='absolute inset-0 bg-black opacity-0 dark:opacity-30 z-50'></div>
        </div>
-       <div className="w-full rounded scale-125 ml-auto dark:scale-100 blur-sm dark:blur-none hover:shadow-xl
+       {/* <div className="w-full rounded scale-125 ml-auto dark:scale-125 blur-sm dark:blur-none hover:shadow-xl
+       transition-all duration-200 ease-in-out"> */}
+           <div className="w-full rounded m-0 scale-125 dark:scale-125 hover:shadow-xl
        transition-all duration-200 ease-in-out">
-           <Image 
+        <Image 
            layout="fixed"
            width={imageWidth}
            height={imageHeight}
@@ -54,6 +62,7 @@ function RadioWindow() {
            src="/radiowindow/aqsanight.jpeg"
            alt="image"
            />
+           <div className='absolute inset-0 bg-black opacity-70 dark:opacity-0 z-50'></div>
        </div>
      </div>
   )
