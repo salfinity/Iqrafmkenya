c695c4233b84dde78b9b33aa378c0b1d63667234
mohsalim <moharts2000@gmail.com>
2023-11-20 23:45:07 +0300

    all

--- app/Slide.tsx
+++ app/Slide.tsx
@@ -1,18 +1,16 @@
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 interface propsType {
   img: string;
-}
+  }
 
 const Slide: React.FC<propsType> = ({ img }) => {
   const imageWidth = 100; // Set a reasonable width for your images
-  const imageHeight = 100;
-  const { theme } = useTheme(); // Use useTheme hook to get the current theme
+  const imageHeight = 100; 
 
   return (
-    <div className={`outline-none border-none relative ${theme === 'dark' ? 'dark' : ''}`}>
+    <div className="outline-none border-none relative">  
       <Image
         className="w-full h-full rounded-xl object-cover"
         src={img}
