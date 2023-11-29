5de8fb73f238ba23dab3dc6f9a6637c520fb7cd4
mohsalim <moharts2000@gmail.com>
2023-11-29 14:23:15 +0300

    all

--- app/management/page.tsx
+++ app/management/page.tsx
@@ -6,6 +6,9 @@ import { RiArrowGoBackFill } from 'react-icons/ri'
 function Page() {
   const imageWidth = 200; // Set a reasonable width for your images
   const imageHeight = 200; // Set a reasonable height for your images
+  const largerimageWidth = 400; // Set a reasonable width for your images
+  const largerimageHeight = 400; // Set a reasonable height for your images
+
 
   return (
     <div className="container items-center mt-3 mx-auto md:px-6">
@@ -14,46 +17,21 @@ function Page() {
           <span className='underline decoration-6 decoration-orange-400'>MANAGE</span> MENT
         </h1>
 
-        <div className="lg:gap-xl-12 grid gap-x-6 md:grid-cols-3 xl:grid-cols-4">
-          <div className="mb-12">
+        <div className="mb-12">
             <Image
               layout="fixed" // Use "fixed" layout to specify width and height
-              width={imageWidth} // Set the width for the image
-              height={imageHeight} // Set the height for the image
-              objectFit="cover"
-              alt=""
-              src="/management/mosque.jpg"
-              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
-            />
-            <p className="mb-2 font-bold">MaashaAllah</p>
-            <p className="text-neutral-500 dark:text-neutral-300">President</p>
-          </div>
-          <div className="mb-12">
-            <Image
-              layout="fixed" // Use "fixed" layout to specify width and height
-              width={imageWidth} // Set the width for the image
-              height={imageHeight} // Set the height for the image
-              objectFit="cover"
-              alt=""
-              src="/management/mosque.jpg"
-              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
-            />
-            <p className="mb-2 font-bold">MaashaAllah</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Vice President</p>
-          </div>
-          <div className="mb-12">
-            <Image
-              layout="fixed" // Use "fixed" layout to specify width and height
-              width={imageWidth} // Set the width for the image
-              height={imageHeight} // Set the height for the image
+              width={largerimageWidth} // Set the width for the image
+              height={largerimageHeight} // Set the height for the image
               objectFit="cover"
               alt=""
               src="/management/bahaidar.jpeg"
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
-            <p className="mb-2 font-bold">Dr. Mohamed Ahmed Bahaidar</p>
-            <p className="text-neutral-500 dark:text-neutral-300">CEO</p>
+            <p className="mb-2 font-bold text-2xl">Dr. Mohamed Ahmed Bahaidar</p>
+            <p className="text-neutral-500 dark:text-neutral-300 text-3xl">CEO</p>
           </div>
+
+        <div className="lg:gap-xl-12 grid gap-x-6 md:grid-cols-3 xl:grid-cols-3">
           <div className="mb-12">
             <Image
               layout="fixed" // Use "fixed" layout to specify width and height
@@ -65,20 +43,7 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Sofia Ibrahim</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Head of Programs</p>
-          </div>
-          <div className="mb-12">
-            <Image
-              layout="fixed" // Use "fixed" layout to specify width and height
-              width={imageWidth} // Set the width for the image
-              height={imageHeight} // Set the height for the image
-              objectFit="cover"
-              alt=""
-              src="/team/galoro.jpeg"
-              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
-            />
-            <p className="mb-2 font-bold">Samira Galoro</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Head of productions</p>
+            <p className="text-neutral-500 dark:text-neutral-300">Head of Productions</p>
           </div>
           <div className="mb-12">
             <Image
@@ -91,8 +56,36 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Khamis Abdallah</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Editor</p>
+            <p className="text-neutral-500 dark:text-neutral-300">Head of staff</p>
           </div>
+          
+          <div className="mb-12">
+            <Image
+              layout="fixed" // Use "fixed" layout to specify width and height
+              width={imageWidth} // Set the width for the image
+              height={imageHeight} // Set the height for the image
+              objectFit="cover"
+              alt=""
+              src="/team/ramlah.png"
+              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
+            />
+            <p className="mb-2 font-bold">Ramla Abdi</p>
+            <p className="text-neutral-500 dark:text-neutral-300">Head of admin</p>
+          </div>
+          <div className="mb-12">
+            <Image
+              layout="fixed" // Use "fixed" layout to specify width and height
+              width={imageWidth} // Set the width for the image
+              height={imageHeight} // Set the height for the image
+              objectFit="cover"
+              alt=""
+              src="/team/galoro.jpeg"
+              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
+            />
+            <p className="mb-2 font-bold">Samira Galoro</p>
+            <p className="text-neutral-500 dark:text-neutral-300">Technical support officer</p>
+          </div>
+          
           <div className="mb-12">
             <Image
               layout="fixed" // Use "fixed" layout to specify width and height
