2b860b9eb9a098422ceb50f79c8e04c45f03d5c8
mohsalim <moharts2000@gmail.com>
2023-12-02 16:09:56 +0300

    picture

--- app/management/page.tsx
+++ app/management/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Link from 'next/link'
-import React from 'react'
-import { RiArrowGoBackFill } from 'react-icons/ri'
+import Link from "next/link";
+import React from "react";
+import { RiArrowGoBackFill } from "react-icons/ri";
 
 function Page() {
   const imageWidth = 200; // Set a reasonable width for your images
@@ -9,27 +9,29 @@ function Page() {
   const largerimageWidth = 400; // Set a reasonable width for your images
   const largerimageHeight = 400; // Set a reasonable height for your images
 
-
   return (
     <div className="container items-center mt-3 mx-auto md:px-6">
       <section className="mb-32 text-center">
-        <h1 className='font-serif md:text-4xl text-2xl text-center mt-3 mb-5'>
-          <span className='underline decoration-6 decoration-orange-400'>MANAGE</span> MENT
+        <h1 className="font-serif md:text-4xl text-2xl text-center mt-3 mb-5">
+          <span className="underline decoration-6 decoration-orange-400">
+            MANAGE
+          </span>{" "}
+          MENT
         </h1>
 
         <div className="mb-12">
-            <Image
-              layout="fixed" // Use "fixed" layout to specify width and height
-              width={largerimageWidth} // Set the width for the image
-              height={largerimageHeight} // Set the height for the image
-              objectFit="cover"
-              alt=""
-              src="/management/bahaidar.jpeg"
-              className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
-            />
-            <p className="mb-2 font-bold text-2xl">Dr. Mohamed Ahmed Bahaidar</p>
-            <p className="text-neutral-500 dark:text-neutral-300 text-3xl">CEO</p>
-          </div>
+          <Image
+            layout="fixed" // Use "fixed" layout to specify width and height
+            width={largerimageWidth} // Set the width for the image
+            height={largerimageHeight} // Set the height for the image
+            objectFit="cover"
+            alt=""
+            src="/management/bahaidar.jpeg"
+            className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
+          />
+          <p className="mb-2 font-bold text-2xl">Dr. Mohamed Ahmed Bahaidar</p>
+          <p className="text-neutral-500 dark:text-neutral-300 text-3xl">CEO</p>
+        </div>
 
         <div className="lg:gap-xl-12 grid gap-x-6 md:grid-cols-3 xl:grid-cols-3">
           <div className="mb-12">
@@ -43,7 +45,9 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Sofia Ibrahim</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Head of Productions</p>
+            <p className="text-neutral-500 dark:text-neutral-300">
+              Head of Productions
+            </p>
           </div>
           <div className="mb-12">
             <Image
@@ -56,9 +60,11 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Khamis Abdallah</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Head of staff</p>
+            <p className="text-neutral-500 dark:text-neutral-300">
+              Head of staff
+            </p>
           </div>
-          
+
           <div className="mb-12">
             <Image
               layout="fixed" // Use "fixed" layout to specify width and height
@@ -66,11 +72,13 @@ function Page() {
               height={imageHeight} // Set the height for the image
               objectFit="cover"
               alt=""
-              src="/team/ramlah.png"
+              src="/management/ramla.jpeg"
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Ramla Abdi</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Head of admin</p>
+            <p className="text-neutral-500 dark:text-neutral-300">
+              Head of admin
+            </p>
           </div>
           <div className="mb-12">
             <Image
@@ -83,9 +91,11 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Samira Galoro</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Technical support officer</p>
+            <p className="text-neutral-500 dark:text-neutral-300">
+              Technical support officer
+            </p>
           </div>
-          
+
           <div className="mb-12">
             <Image
               layout="fixed" // Use "fixed" layout to specify width and height
@@ -97,14 +107,16 @@ function Page() {
               className="mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20"
             />
             <p className="mb-2 font-bold">Mohammed Rajab</p>
-            <p className="text-neutral-500 dark:text-neutral-300">Technical support officer</p>
+            <p className="text-neutral-500 dark:text-neutral-300">
+              Technical support officer
+            </p>
           </div>
           {/* ... Repeat this structure for other team members */}
         </div>
-        <button className='border-4 dark:border-white border-black rounded-md px-4 py-2'>
-          <Link href='/'>
-            <p className='underline text-black dark:text-white cursor-pointer items-center flex pl-8 pr-8 gap-2'>
-              <RiArrowGoBackFill className='text-bold'/> Back
+        <button className="border-4 dark:border-white border-black rounded-md px-4 py-2">
+          <Link href="/">
+            <p className="underline text-black dark:text-white cursor-pointer items-center flex pl-8 pr-8 gap-2">
+              <RiArrowGoBackFill className="text-bold" /> Back
             </p>
           </Link>
         </button>
