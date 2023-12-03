7d1f40ee0c8aad141cfc7b8663782c07c74808f5
mohsalim <moharts2000@gmail.com>
2023-12-03 03:43:26 +0300

    width

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -42,7 +42,7 @@ function HistoryPage() {
         </div>
       </div>
 
-      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
+      <div className="max-w-6xl mx-auto p-2 grid md:grid-cols-5 gap-8 py-8">
         <div className="font-serif col-span-4">
           <p>OUR STORY</p>
           <h2>
