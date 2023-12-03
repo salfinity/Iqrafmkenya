16768180bc10b356bbf729054fbaaec5bdddb10f
mohsalim <moharts2000@gmail.com>
2023-12-03 22:28:13 +0300

    all

--- app/TimeComponent.tsx
+++ app/TimeComponent.tsx
@@ -40,7 +40,7 @@ const TimeComponent: React.FC<TimeComponentProps> = ({ timeZone }) => {
       <span className="font-semibold text-xl">{currentTime} </span>
       <span className="relative flex h-2 w-2">
         <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-500 opacity-75"></span>
-        <span className="relative inline-flex h-3 w-3 rounded-full bg-red-700"></span>
+        <span className="relative inline-flex h-2 w-2 rounded-full bg-red-700"></span>
       </span>
       <p className="text-lg font-serif">Nairobi</p>
     </div>
