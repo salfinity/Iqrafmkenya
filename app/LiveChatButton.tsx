a49ef723ef44efa3326efef436b088d95eb1867b
mohsalim <moharts2000@gmail.com>
2023-12-04 00:55:19 +0300

    all

--- app/LiveChatButton.tsx
+++ app/LiveChatButton.tsx
@@ -1,11 +1,21 @@
+//"use client";
+
+//import router from "next/router";
 import React from "react";
+
+{
+  /*const handlechatClick = () => {
+  router.push("/chatai");
+};*/
+}
 
 export default function LiveChatButton() {
   return (
     <div>
       <button
         className="bg-slate-700 py-3 px-4 h-10 rounded-full text-white dark:text-gray-300
- hover:bg-slate-600 fixed z-50 bottom-5 right-5 border-3 border-slate-900 dark:border-slate-200"
+           hover:bg-slate-600 fixed z-50 bottom-5 right-5 border-3 border-slate-900 dark:border-slate-200"
+        //onClick={handlechatClick}
       >
         <a
           className="group flex justify-center gap-1.5 ltr:sm:justify-start rtl:sm:justify-end"
