857d9196dfb59b1c2823af5e617cdeb0cc0b3e37
mohsalim <moharts2000@gmail.com>
2023-12-13 23:17:10 +0300

    all

--- app/(footerroutes)/adverts/page.tsx
+++ app/(footerroutes)/adverts/page.tsx
@@ -75,7 +75,7 @@ function AdvertsPage() {
         </div>
       </div>
       <button className="border-4 dark:border-white border-black rounded-full px-4 py-2">
-        <Link href="/">
+        <Link href="/Homepage">
           <span className="underline text-black dark:text-white cursor-pointer items-center flex pl-8 pr-8 gap-2">
             <RiArrowGoBackFill className="text-bold" /> Back
           </span>
